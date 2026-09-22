73d17e4706831d1851d2ead8b551f57c5127d637
savio-123 <saviorajan3@gmail.com>
2026-09-22 19:45:32 +0530

    Update service.jsx section

--- src/data/Services.js
+++ src/data/Services.js
@@ -7,31 +7,31 @@ const serviceCategories = [
                 id: 1,
                 name: "Threading",
                 price: 60,
-                image: "/services/threading..jpg"
+                image: "/services/threading.webp"
             },
             {
                 id: 2,
                 name: "Forehead",
                 price: 30,
-                image: "/services/threading..jpg"
+                image: "/services/threading.webp"
             },
             {
                 id: 3,
                 name: "Upper Lip",
                 price: 30,
-                image: "/services/threading..jpg"
+                image: "/services/threading.webp"
             },
             {
                 id: 4,
                 name: "Chin",
                 price: 30,
-                image: "/services/threading..jpg"
+                image: "/services/threading.webp"
             },
             {
                 id: 5,
                 name: "Full Face",
                 price: 150,
-                image: "/services/threading..jpg"
+                image: "/services/threading.webp"
             }
         ]
     },
@@ -44,97 +44,97 @@ const serviceCategories = [
                 id: 6,
                 name: "Full Hand - Normal",
                 price: 750,
-                image: "/services/photo-1634449571010-02389ed0f9b0.avif"
+                image: "/services/waxxing.webp"
             },
             {
                 id: 7,
                 name: "Full Hand - Liposoluble",
                 price: 900,
-                image: "/services/photo-1634449571010-02389ed0f9b0.avif"
+                image: "/services/waxxing.webp"
             },
             {
                 id: 8,
                 name: "Half Hand - Normal",
                 price: 450,
-                image: "/services/photo-1634449571010-02389ed0f9b0.avif"
+                image: "/services/waxxing.webp"
             },
             {
                 id: 9,
                 name: "Half Hand - Liposoluble",
                 price: 700,
-                image: "/services/photo-1634449571010-02389ed0f9b0.avif"
+                image: "/services/waxxing.webp"
             },
             {
                 id: 10,
                 name: "Full Legs - Normal",
                 price: 1200,
-                image: "/services/photo-1634449571010-02389ed0f9b0.avif"
+                image: "/services/waxxing.webp"
             },
             {
                 id: 11,
                 name: "Full Legs - Liposoluble",
                 price: 1400,
-                image: "/services/photo-1634449571010-02389ed0f9b0.avif"
+                image: "/services/waxxing.webp"
             },
             {
                 id: 12,
                 name: "Half Legs - Normal",
                 price: 750,
-                image: "/services/photo-1634449571010-02389ed0f9b0.avif"
+                image: "/services/waxxing.webp"
             },
             {
                 id: 13,
                 name: "Half Legs - Liposoluble",
                 price: 1000,
-                image: "/services/photo-1634449571010-02389ed0f9b0.avif"
+                image: "/services/waxxing.webp"
             },
             {
                 id: 14,
                 name: "Under Arms - Normal",
                 price: 300,
-                image: "/services/photo-1634449571010-02389ed0f9b0.avif"
+                image: "/services/waxxing.webp"
             },
             {
                 id: 15,
                 name: "Under Arms - Liposoluble",
                 price: 400,
-                image: "/services/photo-1634449571010-02389ed0f9b0.avif"
+                image: "/services/waxxing.webp"
             },
             {
                 id: 16,
                 name: "Chin",
                 price: 100,
-                image: "/services/photo-1634449571010-02389ed0f9b0.avif"
+                image: "/services/waxxing.webp"
             },
             {
                 id: 17,
                 name: "Full Face",
                 price: 250,
-                image: "/services/photo-1634449571010-02389ed0f9b0.avif"
+                image: "/services/waxxing.webp"
             },
             {
                 id: 18,
                 name: "Full Body - Normal",
                 price: 3000,
-                image: "/services/photo-1634449571010-02389ed0f9b0.avif"
+                image: "/services/waxxing.webp"
             },
             {
                 id: 19,
                 name: "Full Body - Liposoluble",
                 price: 4000,
-                image: "/services/photo-1634449571010-02389ed0f9b0.avif"
+                image: "/services/waxxing.webp"
             },
             {
                 id: 20,
                 name: "Back Neck",
                 price: 200,
-                image: "/services/photo-1634449571010-02389ed0f9b0.avif"
+                image: "/services/waxxing.webp"
             },
             {
                 id: 21,
                 name: "Upper Lip",
                 price: 100,
-                image: "/services/photo-1634449571010-02389ed0f9b0.avif"
+                image: "/services/waxxing.webp"
             }
         ]
     },
@@ -147,37 +147,37 @@ const serviceCategories = [
                 id: 22,
                 name: "Face",
                 price: 550,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/bleach.webp"
             },
             {
                 id: 23,
                 name: "Hand Full",
                 price: 1000,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/bleach.webp"
             },
             {
                 id: 24,
                 name: "Hand Half",
                 price: 700,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/bleach.webp"
             },
             {
                 id: 25,
                 name: "Neck & Back",
                 price: 700,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/bleach.webp"
             },
             {
                 id: 26,
                 name: "Legs",
                 price: 1500,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/bleach.webp"
             },
             {
                 id: 27,
                 name: "Full Body",
                 price: 3500,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/bleach.webp"
             }
         ]
     },
@@ -190,49 +190,49 @@ const serviceCategories = [
                 id: 28,
                 name: "Normal",
                 price: 500,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/cleanup.webp"
             },
             {
                 id: 29,
                 name: "Papaya",
                 price: 600,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/cleanup.webp"
             },
             {
                 id: 30,
                 name: "Orange",
                 price: 650,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/cleanup.webp"
             },
             {
                 id: 31,
                 name: "Pearl",
                 price: 750,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/cleanup.webp"
             },
             {
                 id: 32,
                 name: "Fruit",
                 price: 500,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/cleanup.webp"
             },
             {
                 id: 33,
                 name: "Diamond",
                 price: 1000,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/cleanup.webp"
             },
             {
                 id: 34,
                 name: "O3 Clean Up",
                 price: 1100,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/cleanup.webp"
             },
             {
                 id: 35,
                 name: "Purifying Clean Up",
                 price: 550,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/cleanup.webp"
             }
         ]
     },
@@ -245,109 +245,109 @@ const serviceCategories = [
                 id: 36,
                 name: "Normal Facial",
                 price: 600,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/facial.webp"
             },
             {
                 id: 37,
                 name: "Orange Facial",
                 price: 800,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/facial.webp"
             },
             {
                 id: 38,
                 name: "Papaya Facial",
                 price: 900,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/facial.webp"
             },
             {
                 id: 39,
                 name: "Brightening Facial",
                 price: 1800,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/facial.webp"
             },
             {
                 id: 40,
                 name: "Whitening Facial",
                 price: 1300,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/facial.webp"
             },
             {
                 id: 41,
                 name: "Veg Peel",
                 price: 800,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/facial.webp"
             },
             {
                 id: 42,
                 name: "Pearl Facial",
                 price: 1500,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/facial.webp"
             },
             {
                 id: 43,
                 name: "Diamond Facial",
                 price: 2000,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/facial.webp"
             },
             {
                 id: 44,
                 name: "O3 Purifying Facial",
                 price: 2200,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/facial.webp"
             },
             {
                 id: 45,
                 name: "Pigmentation Facial",
                 price: 1100,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/facial.webp"
             },
             {
                 id: 46,
                 name: "D Tan Facial",
                 price: 1400,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/facial.webp"
             },
             {
                 id: 47,
                 name: "BB Glow Facial",
                 price: 1400,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/facial.webp"
             },
             {
                 id: 48,
                 name: "Fruit Facial",
                 price: 1200,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/facial.webp"
             },
             {
                 id: 49,
                 name: "Green Tea Facial",
                 price: 1300,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/facial.webp"
             },
             {
                 id: 50,
                 name: "Platinum Facial",
                 price: 1700,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/facial.webp"
             },
             {
                 id: 51,
                 name: "Gold Facial",
                 price: 2400,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/facial.webp"
             },
             {
                 id: 52,
                 name: "Fairness Facial",
                 price: 1500,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/facial.webp"
             },
             {
                 id: 53,
                 name: "Sebum Control Facial",
                 price: 1600,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/facial.webp"
             }
         ]
     },
@@ -360,13 +360,13 @@ const serviceCategories = [
                 id: 54,
                 name: "Hydrating Body Polish",
                 price: 4000,
-                image: "/services/photo-1634449571010-02389ed0f9b0.avif"
+                image: "/services/body_polish.webp"
             },
             {
                 id: 55,
                 name: "Whitening Body Polish",
                 price: 5000,
-                image: "/services/photo-1634449571010-02389ed0f9b0.avif"
+                image: "/services/body_polish.webp"
             }
         ]
     },
@@ -379,37 +379,37 @@ const serviceCategories = [
                 id: 56,
                 name: "D Tan",
                 price: 600,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/facial.webp"
             },
             {
                 id: 57,
                 name: "D Tan Half Leg",
                 price: 900,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/facial.webp"
             },
             {
                 id: 58,
                 name: "D Tan Full Leg",
                 price: 1600,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/facial.webp"
             },
             {
                 id: 59,
                 name: "D Tan Half Hand",
                 price: 800,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/facial.webp"
             },
             {
                 id: 60,
                 name: "D Tan Full Hand",
                 price: 1100,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/facial.webp"
             },
             {
                 id: 61,
                 name: "O3 D Tan",
                 price: 700,
-                image: "/services/Ifeoma_-_Facial_b34e99d4-7a79-442e-892b-1757ea829a3e.webp"
+                image: "/services/facial.webp"
             }
         ]
     },
@@ -422,19 +422,19 @@ const serviceCategories = [
                 id: 62,
                 name: "Normal Manicure",
                 price: 750,
-                image: "/services/pedicre.webp"
+                image: "/services/manicure.webp"
             },
             {
                 id: 63,
                 name: "Spa Manicure",
                 price: 800,
-                image: "/services/pedicre.webp"
+                image: "/services/manicure.webp"
             },
             {
                 id: 64,
                 name: "French Manicure",
                 price: 1000,
-                image: "/services/pedicre.webp"
+                image: "/services/manicure.webp"
             }
         ]
     },
@@ -447,19 +447,19 @@ const serviceCategories = [
                 id: 65,
                 name: "Normal Pedicure",
                 price: 1000,
-                image: "/services/pedicre.webp"
+                image: "/services/pedicure.webp"
             },
             {
                 id: 66,
                 name: "Spa Pedicure",
                 price: 1200,
-                image: "/services/pedicre.webp"
+                image: "/services/pedicure.webp"
             },
             {
                 id: 67,
                 name: "Polishing Pedicure",
                 price: 1500,
-                image: "/services/pedicre.webp"
+                image: "/services/pedicure.webp"
             }
         ]
     },
